7a04c758f74f6ffa6008a86c42444b2f60f62c90
Bartosz Skrzypczak <Bartek_s2@wp.pl>
2014-11-15 18:33:39 +0100

    Updated licence headers.

--- src/main/java/com/github/barteks2x/cubit/render/TextureCoords.java
+++ src/main/java/com/github/barteks2x/cubit/render/TextureCoords.java
@@ -1,7 +1,7 @@
-/*
+/* 
  * The MIT License
  *
- * Copyright 2014 bartosz.
+ * Copyright 2014 Bartosz Skrzypczak.
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
